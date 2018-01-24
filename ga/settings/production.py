a1c6c9d4a05772b41f7023e9e944925ad6b733dd
oromay <orormay@yandex.ru>
2018-01-24 11:35:45 +0300

    security

--- ga/settings/production.py
+++ ga/settings/production.py
@@ -5,7 +5,7 @@ DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1','smtp.gmail.com', 'alog.post@gmail.com', '.golosa-afriki.ru']
 
 #advised by manage.py check --deploy
-SECURE_CONTENT_TYPE_NOSNIFF = True
+# SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True

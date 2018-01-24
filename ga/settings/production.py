db4001776c64b251610ad75605e3c8eea2ab797d
oromay <orormay@yandex.ru>
2018-01-24 11:27:12 +0300

    Security changes in settings (advised by manage.py check --deploy)

--- ga/settings/production.py
+++ ga/settings/production.py
@@ -4,6 +4,14 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','smtp.gmail.com', 'alog.post@gmail.com', '.golosa-afriki.ru']
 
+#advised by manage.py check --deploy
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+X_FRAME_OPTIONS = 'DENY'
+
+
 INSTALLED_APPS += ('lockdown', )
 MIDDLEWARE += ('lockdown.middleware.LockdownMiddleware', )
 LOCKDOWN_PASSWORDS = ('nyala', )
